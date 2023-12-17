290ad091f90db0f94899e65dc4f1d487a6adb824
nehemiahcode <codehubby@gmail.com>
2023-12-17 06:47:42 +0100

    updated searchUsers

--- components/shared/Searchbar.tsx
+++ components/shared/Searchbar.tsx
@@ -41,7 +41,7 @@ function Searchbar({ routeType }: Props) {
         value={search}
         onChange={(e) => setSearch(e.target.value)}
         placeholder={`${
-          routeType !== "/search" ? "Search communities" : "Search creators"
+          routeType !== "/search" ? "Search Users" : "Search Users"
         }`}
         className='no-focus searchbar_input'
       />
